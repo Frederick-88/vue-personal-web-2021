27b60d441d0263231feb0ded2c54e27fdb02a6cf
Chen Frederick <frederickfd88@gmail.com>
2021-04-17 23:09:38 +0700

    Moved Layout to Highest Component Level

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,56 @@
-<template>
-  <div id="app">
-    <router-view />
-  </div>
+<template lang="pug">
+  div#app
+    main.main-section(:style="backgroundImage")   
+      div.main-section__container
+        Navbar.navigation__bar
+        router-view
+      Sidebar.sidebar
 </template>
 
 <script>
+import { mapState, mapMutations } from "vuex";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+
 export default {
   metaInfo: {
     title: "Chen Frederick's -- Website",
+  },
+  components: {
+    Navbar,
+    Sidebar,
+  },
+  computed: {
+    ...mapState(["webTheme"]),
+    backgroundImage() {
+      // creating black/white overlay effect
+      const darkBackground = {
+        backgroundImage:
+          "linear-gradient(to bottom, rgba(0,0,0,0.35) 0%,rgba(0,0,0,0.35) 100%), url(" +
+          require("@/assets/images/wallpaper-dark.jpg") +
+          ")",
+      };
+      const lightBackground = {
+        backgroundImage:
+          "linear-gradient(to bottom, rgba(255,255,255,0.65) 0%,rgba(255,255,255,0.65) 100%), url(" +
+          require("@/assets/images/wallpaper-white.jpg") +
+          ")",
+      };
+      return this.webTheme === "darkMode" ? darkBackground : lightBackground;
+    },
+  },
+  methods: {
+    ...mapMutations(["setWebTheme"]),
+  },
+  mounted() {
+    let localTheme = localStorage.getItem("theme");
+    if (!localTheme) {
+      this.setWebTheme("");
+    } else {
+      this.setWebTheme("darkMode");
+    }
+    // documentElement select the root tag of our html which is <html/>
+    document.documentElement.setAttribute("data-theme", localTheme);
   },
 };
 </script>
@@ -35,6 +78,41 @@ body {
 
   * {
     box-sizing: border-box;
+  }
+
+  .main-section {
+    background-attachment: static;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    width: 100%;
+    height: 100vh;
+    position: relative;
+
+    .sidebar {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      box-sizing: border-box;
+      position: fixed;
+      height: 100%;
+      z-index: 10;
+      padding: 40px 15px 0;
+      width: 100px;
+      right: 0;
+    }
+  }
+
+  .main-section__container {
+    position: absolute;
+    width: 100%;
+    min-height: 100vh;
+    width: calc(100% - 100px);
+
+    .navigation__bar {
+      padding: 40px 130px 0;
+      display: flex;
+    }
   }
 }
 </style>
