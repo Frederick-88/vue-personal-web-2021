a0c9eac219a85fdb026e02dcfa9162745054323e
Chen Frederick <frederickfd88@gmail.com>
2021-04-27 22:23:23 +0700

    Completed Skills Section ( with darkmode )

---
+++ src/components/Home/HomeSkills.vue
@@ -0,0 +1,167 @@
+<template lang="pug">
+  section.home-skills__content
+    h4.header-title My Skills - Services
+    div.skills-carousel__container
+      div.carousel-arrow__buttons
+        i.carousel-arrow.icon-arrow-left(@click="showPrev")
+        i.carousel-arrow.icon-arrow-right(@click="showNext")
+      VueSlickCarousel.skills-carousel(v-bind="settings" ref="carousel")
+        div.skills__card-lists(v-for="skill in SkillLists")
+          div.card-list
+            i.card-icon(:class="skill.icon")
+            h2.title {{skill.title}}
+</template>
+
+<script>
+import SkillLists from "@/utilities/SkillLists";
+
+export default {
+  name: "HomeSkills",
+  data() {
+    return {
+      SkillLists: SkillLists,
+      settings: {
+        arrows: false,
+        dots: true,
+        infinite: false,
+        rows: 2,
+        speed: 500,
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        initialSlide: 0,
+        responsive: [
+          {
+            breakpoint: 1366,
+            settings: {
+              slidesToShow: 5,
+              slidesToScroll: 5,
+              infinite: false,
+              dots: true,
+            },
+          },
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 4,
+              slidesToScroll: 4,
+              infinite: false,
+              dots: true,
+            },
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: false,
+            },
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2,
+              infinite: false,
+            },
+          },
+        ],
+      },
+    };
+  },
+  methods: {
+    showNext() {
+      this.$refs.carousel.next();
+    },
+    showPrev() {
+      this.$refs.carousel.prev();
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.home-skills__content {
+  margin: 50px 100px 0;
+
+  .header-title {
+    text-align: end;
+    font-weight: 500;
+    color: var(--text-color-primary);
+    margin-right: 30px;
+    font-size: 0.875rem;
+    text-transform: uppercase;
+  }
+}
+
+.skills-carousel__container {
+  margin: 20px 0 30px;
+  display: flex;
+
+  div.carousel-arrow__buttons {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 10%;
+
+    .carousel-arrow {
+      padding: 10px;
+      color: var(--button-text);
+      background: var(--button-solid-background);
+      transition: background 0.2s, color 0.2s;
+      border-radius: 4px;
+      margin: 10px 0;
+      cursor: pointer;
+
+      &:hover {
+        background: var(--button-text);
+        color: var(--button-solid-background);
+      }
+    }
+  }
+
+  .skills-carousel {
+    width: 90%;
+  }
+
+  .card-list {
+    padding: 20px;
+    min-height: 125px;
+    background: var(--text-color-primary);
+    color: var(--white-dark);
+    border-radius: 4px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    flex-direction: column;
+    margin: 10px;
+
+    .card-icon {
+      font-size: 1.5rem;
+      margin-bottom: 10px;
+
+      &.icon-mysql {
+        font-size: 2rem;
+      }
+    }
+
+    .title {
+      font-size: 0.875rem;
+      font-weight: 600;
+    }
+  }
+
+  // // rewrite vue slick carousel stylings
+  .slick-dots {
+    bottom: -40px;
+
+    li,
+    li.slick-active {
+      button:before {
+        color: var(--dark-white);
+      }
+    }
+  }
+}
+</style>
---
+++ src/utilities/SkillLists.js
@@ -0,0 +1,114 @@
+module.exports = [
+  {
+    title: "Vue JS",
+    icon: "icon-vue",
+  },
+  {
+    title: "Nuxt JS",
+    icon: "icon-nuxt",
+  },
+  {
+    title: "Vuex",
+    icon: "icon-vue",
+  },
+  {
+    title: "React Redux & Hooks",
+    icon: "icon-redux",
+  },
+  {
+    title: "React JS",
+    icon: "icon-react",
+  },
+  {
+    title: "SEO & Web Performance",
+    icon: "icon-earth2",
+  },
+  {
+    title: "Github",
+    icon: "icon-github",
+  },
+  {
+    title: "Bitbucket",
+    icon: "icon-bitbucket",
+  },
+  {
+    title: "AWS",
+    icon: "icon-amazonaws",
+  },
+  {
+    title: "Cloudflare + Web Deployment",
+    icon: "icon-cloudflare",
+  },
+  {
+    title: "SCSS",
+    icon: "icon-css",
+  },
+  {
+    title: "Linux / UNIX System",
+    icon: "icon-linux",
+  },
+  {
+    title: "Mongo DB",
+    icon: "icon-mongodb",
+  },
+  {
+    title: "Express JS",
+    icon: "icon-javascript",
+  },
+  {
+    title: "Node JS",
+    icon: "icon-node-js",
+  },
+  {
+    title: "PHP",
+    icon: "icon-php",
+  },
+  {
+    title: "MY SQL",
+    icon: "icon-mysql",
+  },
+  {
+    title: "Fullstack Development",
+    icon: "icon-code",
+  },
+  {
+    title: "Frontend Development",
+    icon: "icon-design",
+  },
+  {
+    title: "Responsive Web Design",
+    icon: "icon-responsive",
+  },
+  {
+    title: "Backend Development",
+    icon: "icon-database",
+  },
+  {
+    title: "REST API",
+    icon: "icon-earth",
+  },
+  {
+    title: "API Building",
+    icon: "icon-earth",
+  },
+  {
+    title: "Agile Scrum",
+    icon: "icon-team",
+  },
+  {
+    title: "Interior Design",
+    icon: "icon-design",
+  },
+  {
+    title: "Graphic Design",
+    icon: "icon-photoshop",
+  },
+  {
+    title: "Teamwork",
+    icon: "icon-team",
+  },
+  {
+    title: "English",
+    icon: "icon-language",
+  },
+];
