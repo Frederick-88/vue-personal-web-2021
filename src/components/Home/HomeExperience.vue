222d59ee4d2b95354c7fa6395d377d49f01dbe45
Chen Frederick <frederickfd88@gmail.com>
2021-05-06 22:55:47 +0700

    Update Experience

--- src/components/Home/HomeExperience.vue
+++ src/components/Home/HomeExperience.vue
@@ -30,6 +30,46 @@
 <script>
 export default {
   name: "HomeExperience",
+  data() {
+    return {
+      experienceLists: [
+        {
+          title: "Infinity Batam ( eXotica Batam )",
+          logo: require("@/assets/images/works/weatherpedia-pwa1.png"),
+          description:
+            "As Senior Graphic Designer, creating 3D Interior & Graphic Designs creations, managing Junior team, held meetings with big companies in batam like PT JOTUN, SIIX, PHILIPS, etc.",
+          keyContributions: ["Graphic Design", "Interior Design"],
+        },
+        {
+          title: "OFFEO SG",
+          logo: require("@/assets/images/works/weatherpedia-pwa1.png"),
+          description: "Junior Frontend & Frontend Developer",
+          keyContributions: ["Graphic Design", "Interior Design"],
+        },
+        {
+          title: "Impact Byte Fullstack Bootcamp",
+          logo: require("@/assets/images/works/weatherpedia-pwa1.png"),
+          description:
+            "Intensive Bootcamp in Fullstack Major, Learnt Frontend & Backend Lessons. Learnt ReactJS, ReactRedux, ExpressJS, Javascript, Bootstrap, Etc.",
+          keyContributions: ["Graphic Design", "Interior Design"],
+        },
+        {
+          title: "Fingerfast Laboratory",
+          logo: require("@/assets/images/works/weatherpedia-pwa1.png"),
+          description:
+            "As Intern Graphic Designer, creating Designs, create videos, collaborate in online-queue-ticket-booking application for hospital, create advertisements to support our team better.",
+          keyContributions: ["Graphic Design", "Interior Design"],
+        },
+        {
+          title: "Leader of Maitreyawira's English Debate Club",
+          logo: require("@/assets/images/works/weatherpedia-pwa1.png"),
+          description:
+            "Managing Juniors, improve talents, participated in several competitions with teammates, develop this team to their best versions.",
+          keyContributions: ["Graphic Design", "Interior Design"],
+        },
+      ],
+    };
+  },
 };
 </script>
 
