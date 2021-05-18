32ad177f1d1324faafb9f4cf93d2db61656463bb
Chen Frederick <frederickfd88@gmail.com>
2021-05-18 23:04:40 +0700

    Setting up Device Size Mixin for Global Usage

--- vue.config.js
+++ vue.config.js
@@ -6,6 +6,8 @@ module.exports = {
       sass: {
         data: `
             @import '@/assets/styles/variables.scss';
+            @import '@/assets/styles/deviceSizes.scss';
+            @import '@/assets/icomoon/variables.scss';
             @import '@/assets/icomoon/variables.scss';
           `,
       },
