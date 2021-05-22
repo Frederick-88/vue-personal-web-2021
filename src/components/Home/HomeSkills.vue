f27f9978549d475c107c553e1145d672f50aeabb
Chen Frederick <frederickfd88@gmail.com>
2021-05-22 16:13:59 +0700

    Fixed Carousel swiped too much element issue

--- src/components/Home/HomeSkills.vue
+++ src/components/Home/HomeSkills.vue
@@ -34,42 +34,6 @@ export default {
         slidesToShow: 6,
         slidesToScroll: 1,
         initialSlide: 0,
-        responsive: [
-          {
-            breakpoint: 1366,
-            settings: {
-              slidesToShow: 5,
-              slidesToScroll: 5,
-              infinite: false,
-              dots: true,
-            },
-          },
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 4,
-              slidesToScroll: 4,
-              infinite: false,
-              dots: true,
-            },
-          },
-          {
-            breakpoint: 600,
-            settings: {
-              slidesToShow: 3,
-              slidesToScroll: 3,
-              infinite: false,
-            },
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 2,
-              infinite: false,
-            },
-          },
-        ],
       },
     };
   },
