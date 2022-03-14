783028b29e49a9e189e820dc4636585baebe2c37
Chen Frederick <frederickfd88@gmail.com>
2022-03-14 22:23:55 +0700

    Added Vuex Store Module Practices Example

---
+++ src/store/example-store.js
@@ -0,0 +1,10 @@
+const exampleStore = {
+  namespaced: true, // namespacing reason -> https://stackoverflow.com/questions/47792212/what-exactly-is-namespacing-of-modules-in-vuex
+  state: {
+    stateFromExampleStore: "",
+  },
+  getters: {},
+  mutations: {},
+  actions: {},
+};
+export default exampleStore;
