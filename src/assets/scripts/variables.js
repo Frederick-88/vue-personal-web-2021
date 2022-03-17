88734e9c0031f347b44eb1ca4f46d8eea55e55f9
Chen Frederick <frederickfd88@gmail.com>
2022-03-17 22:08:28 +0700

    Added Variables Scripts for Works Search Keywords Experience

---
+++ src/assets/scripts/variables.js
@@ -0,0 +1,14 @@
+const portofolioKeywords = [
+  "vuejs",
+  "reactjs",
+  "laravel",
+  "php",
+  "mysql",
+  "mongodb",
+  "expressjs",
+  "firebase",
+  "fullstack",
+  "responsive web app",
+];
+
+export { portofolioKeywords };
