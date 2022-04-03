31cd0db523b24eb64481574d4c40e85e68f15a41
Chen Frederick <frederickfd88@gmail.com>
2022-04-03 22:22:19 +0700

    Small-Desktop Responsive Updates : HomeSkills

--- src/components/Home/HomeSkills.vue
+++ src/components/Home/HomeSkills.vue
@@ -28,12 +28,20 @@ export default {
         arrows: false,
         dots: true,
         infinite: false,
-        swipe: false,
+        swipeToSlide: true,
         rows: 2,
         speed: 500,
         slidesToShow: 6,
         slidesToScroll: 2,
         initialSlide: 0,
+        responsive: [
+          {
+            breakpoint: 1366,
+            settings: {
+              slidesToShow: 5,
+            },
+          },
+        ],
       },
     };
   },
@@ -50,7 +58,9 @@ export default {
 
 <style lang="scss">
 .home-skills__content {
-  margin: 50px 100px 0;
+  margin: 100px auto;
+  width: 85%;
+  max-width: 1400px;
 
   .subtitle {
     font-weight: 600;
@@ -70,96 +80,110 @@ export default {
     text-align: center;
     margin-left: 70px;
   }
-}
 
-.skills-carousel__container {
-  margin: 20px 0 30px;
-  display: flex;
-
-  div.carousel-arrow__buttons {
+  .skills-carousel__container {
+    margin: 20px 0 30px;
     display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    width: 10%;
 
-    .carousel-arrow {
-      padding: 10px;
-      color: var(--button-text);
-      background: var(--button-solid-background);
-      transition: background 0.2s, color 0.2s;
+    div.carousel-arrow__buttons {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      width: 100px;
+
+      .carousel-arrow {
+        padding: 10px;
+        color: var(--button-text);
+        background: var(--button-solid-background);
+        transition: background 0.2s, color 0.2s;
+        border-radius: 4px;
+        margin: 10px 0;
+        cursor: pointer;
+
+        &:hover {
+          background: var(--button-text);
+          color: var(--button-solid-background);
+        }
+      }
+    }
+
+    .skills-carousel {
+      width: calc(100% - 100px);
+    }
+
+    .card-list {
+      position: relative;
+      padding: 20px;
+      min-height: 125px;
+      background: var(--dark-white);
+      color: var(--white-dark);
       border-radius: 4px;
-      margin: 10px 0;
-      cursor: pointer;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      text-align: center;
+      flex-direction: column;
+      margin: 10px;
 
-      &:hover {
-        background: var(--button-text);
-        color: var(--button-solid-background);
+      .card-icon {
+        font-size: 1.5rem;
+        margin-bottom: 10px;
+
+        &.icon-mysql {
+          font-size: 2rem;
+        }
+      }
+
+      .card-title {
+        font-size: 0.875rem;
+        font-weight: 600;
+      }
+
+      .level-text {
+        position: absolute;
+        top: -3px;
+        right: -3px;
+        font-size: 0.75rem;
+        font-weight: 600;
+        border-radius: 4px;
+        padding: 5px 10px;
+        color: $white;
+
+        &.text--green {
+          background: $primarySolid;
+        }
+
+        &.text--orange {
+          background: $orange;
+        }
+      }
+    }
+
+    // rewrite vue slick carousel stylings
+    .slick-dots {
+      bottom: -40px;
+
+      li,
+      li.slick-active {
+        button:before {
+          color: var(--dark-white);
+        }
       }
     }
   }
 
-  .skills-carousel {
-    width: 90%;
+  @include large-monitor {
+    max-width: 1700px;
   }
 
-  .card-list {
-    position: relative;
-    padding: 20px;
-    min-height: 125px;
-    background: var(--dark-white);
-    color: var(--white-dark);
-    border-radius: 4px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    text-align: center;
-    flex-direction: column;
-    margin: 10px;
+  @include desktop {
+    .subtitle {
+      font-size: 0.8125rem;
+    }
 
-    .card-icon {
+    .title {
       font-size: 1.5rem;
-      margin-bottom: 10px;
-
-      &.icon-mysql {
-        font-size: 2rem;
-      }
-    }
-
-    .card-title {
-      font-size: 0.875rem;
-      font-weight: 600;
-    }
-
-    .level-text {
-      position: absolute;
-      top: -3px;
-      right: -3px;
-      font-size: 0.75rem;
-      font-weight: 600;
-      border-radius: 4px;
-      padding: 5px 10px;
-      color: $white;
-
-      &.text--green {
-        background: $primarySolid;
-      }
-
-      &.text--orange {
-        background: $orange;
-      }
-    }
-  }
-
-  // rewrite vue slick carousel stylings
-  .slick-dots {
-    bottom: -40px;
-
-    li,
-    li.slick-active {
-      button:before {
-        color: var(--dark-white);
-      }
     }
   }
 }
